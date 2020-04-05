10576ee4a582fc4a1a9c3d1b2272f8e155191755
anuraagz <44057782+anuraagz@users.noreply.github.com>
2020-04-05 20:29:34 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
- 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // component imports
 import HomePageComponent from "./components/homepage.component"
 import InputComponent from "./components/input.component"
 import DataComponent from "./components/data.component"
- 
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
